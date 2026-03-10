1b54c76f35a18f4c561e453adad85112e3b8f7df
Carmelo Guadalajara <carmeloguadalajara48@gmail.com>
2026-03-10 13:38:30 +0000

    File changes

--- src/pages/AbsoluteGauntletPage.jsx
+++ src/pages/AbsoluteGauntletPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useCourse } from '../components/course/CourseContext';
 import { useProfile } from '../components/profiles/ProfileContext';
 import {
